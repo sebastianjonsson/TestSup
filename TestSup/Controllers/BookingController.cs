58ca7e98fb5ae7534ae35f132c7460bbab0b02c0
Sebbe <sebastian.jonsson93@hotmail.com>
2019-03-18 14:08:16 +0100

    wfwfwe

--- TestSup/Controllers/BookingController.cs
+++ TestSup/Controllers/BookingController.cs
@@ -42,7 +42,7 @@ namespace TestSup.Controllers
             }
             catch
             {
-
+                return View();
             }
             return RedirectToAction("Index", "Booking", new { Id = id });
         }
