dde67e3ab4c3a3fcf272c254c8c3b308c86a5394
Sebbe <sebastian.jonsson93@hotmail.com>
2019-05-15 15:29:54 +0200

    Validate editBooking

--- Logic/Bookings.cs
+++ Logic/Bookings.cs
@@ -11,18 +11,25 @@ namespace Logic
     public class Bookings
     {
         public int Id { get; set; }
+        [Required]
         [Display(Name = "Namn")]
         public string UserName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         [Display(Name = "Telefonnummer")]
         public string UserMobile { get; set; }
+        [Required]
         [Display(Name = "Detaljer")]
         public string Subject { get; set; }
+        [Required]
         [Display(Name = "Datum")]
         [DisplayFormat(DataFormatString = "{0:dd-MMM-yy}")]
         public DateTime Date { get; set; }
+        [Required]
         [Display(Name = "Starttid")]
         public string StartTime { get; set; }
+        [Required]
         [Display(Name = "Sluttid")]
         public string Endtime { get; set; }
 
